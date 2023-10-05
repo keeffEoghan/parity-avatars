4983423315272e3de2fe69de29aa8e7be27db005
Eoghan O'Keeffe | epok.tech <epok.tech@gmail.com>
2023-10-06 00:34:48 +0100

    Post

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import getContext from 'pex-context';
 import getRenderer from 'pex-renderer';
 import { load } from 'pex-io';
+import vec3 from 'pex-math/vec3';
 import parseHDR from 'parse-hdr';
 import { map } from '@epok.tech/fn-lists/map';
 
 const { max, PI: pi } = Math;
+const distance3 = vec3.distance;
 
 // For `pex-renderer`'s `gltf` loader to play nicely with `parcel`'s asset hash.
 const gltfURL = (url) => url.href.replace(/\?.*$/, '');
 
 (async () => {
   const canvas = document.querySelector('canvas');
-  const context = getContext({ canvas });
+
+  const context = getContext({
+    canvas//, pixelRatio: max(self.devicePixelRatio, 1.5) || 1.5
+  });
+
   const { gl, PixelFormat, Encoding } = context;
   const renderer = getRenderer({ ctx: context, pauseOnBlur: true });
 
@@ -38,20 +44,27 @@ const gltfURL = (url) => url.href.replace(/\?.*$/, '');
     ],
     0));
 
-  context.set({ pixelRatio: max(self.devicePixelRatio, 1.5) || 1.5 });
-
-  const camera = renderer.camera({ fov: pi*0.5, near: 0.1, far: 1e2 });
+  const camera = renderer.camera({
+    fov: pi*0.5, near: 0.1, far: 1e2, fStop: 1.2, sensorFit: 'overscan'
+  });
 
   const orbiter = renderer.orbiter({
     target: [0, 0.2, 0], position: [0.3, 0.3, 0.6], lat: 0, lon: pi*0.5,
     easing: 5e-2, minDistance: 0.4, maxDistance: 1, pan: false, element: canvas
   });
 
-  const cameraOrbiter = renderer.entity([
-    camera, orbiter, renderer.transform({ position: [0, 0, 3] })
+  const post = renderer.postProcessing({
+    fxaa: true,
+    ssao: true,
+    dof: true,
+    bloom: true, bloomThreshold: 1, bloomRadius: 0.5
+  });
+
+  const viewer = renderer.entity([
+    camera, orbiter, post, renderer.transform({ position: [0, 0, 3] })
   ]);
 
-  renderer.add(cameraOrbiter);
+  renderer.add(viewer);
 
   const sdfMaterial = renderer.material({
     baseColorMap: context.texture2D(sdfImage), metallic: 1, roughness: 0.1
@@ -72,21 +85,35 @@ const gltfURL = (url) => url.href.replace(/\?.*$/, '');
     pixelFormat: PixelFormat.RGBA32F, encoding: Encoding.Linear
   });
 
-  const skybox = renderer.skybox({ texture: skyTexture, backgroundBlur: 1 });
-  const reflectionProbe = renderer.reflectionProbe();
+  const skybox = renderer.skybox({
+    sunPosition: [1, 1, 1], texture: skyTexture, backgroundBlur: 1
+  });
 
-  renderer.add(renderer.entity([skybox, reflectionProbe]));
+  const reflector = renderer.reflectionProbe();
+
+  renderer.add(renderer.entity([skybox, reflector]));
 
   function resize() {
-    context.set({ width: innerWidth, height: innerHeight });
-    camera.set({ aspect: innerWidth/innerHeight });
+    const width = self.innerWidth;
+    const height = self.innerHeight;
+    const { pixelRatio: pr } = context;
+
+    context.set({ width, height });
+    camera.set({ viewport: [0, 0, width*pr, height*pr] });
     renderer.draw();
   }
 
   resize();
   addEventListener('resize', resize);
 
-  context.frame(() => renderer.draw());
+  context.frame(() => {
+    const d = distance3(renderer.root.transform.worldPosition,
+      viewer.transform.worldPosition);
+
+    (post.dofFocusDistance !== d) && post.set({ dofFocusDistance: d });
+
+    renderer.draw();
+  });
 
   self.context = context;
   self.renderer = renderer;
