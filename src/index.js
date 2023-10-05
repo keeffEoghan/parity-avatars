15eef9eb8e74c2d5f0dd75c0194738a51d0abd8e
Eoghan O'Keeffe | epok.tech <epok.tech@gmail.com>
2023-10-05 15:25:11 +0100

    More scene setup and trying Pex, media assets

--- src/index.js
+++ src/index.js
@@ -11,24 +11,35 @@ const renderer = getRenderer({ ctx: context, pauseOnBlur: true });
 
 context.set({ pixelRatio: max(devicePixelRatio, 1.5) || 1.5 });
 
-const camera = renderer.entity([
-  renderer.transform({ position: [0, 0, 3] }),
-  renderer.camera({ fov: pi*0.5, near: 0.1, far: 1e2 })
+const camera = renderer.camera({ fov: pi*0.5, near: 0.1, far: 1e2 });
+
+const orbiter = renderer.orbiter({
+  target: [0, 0, 0], position: [0.3, 0.3, 0.8], lat: 0, lon: pi*0.5, easing: 5e-2,
+  minDistance: 0.2, maxDistance: 1, pan: false, element: canvas
+});
+
+const cameraOrbiter = renderer.entity([
+  camera, orbiter, renderer.transform({ position: [0, 0, 3] })
 ]);
 
-renderer.add(camera);
+renderer.add(cameraOrbiter);
+
+const imageSDF = new Image();
+const textureSDF = context.texture2D({});
+
+imageSDF.onload = () => textureSDF._update(context, textureSDF, imageSDF);
+imageSDF.onerror = console.error;
+imageSDF.src = new URL('../media/volume-sdf.png', import.meta.url);
 
 const cube = renderer.entity([
   renderer.transform({ position: [0, 0, 0] }),
-  renderer.geometry(getSphere(1)),
-  renderer.material({ baseColor: [1, 0, 0, 1] })
+  renderer.geometry(getSphere(0.1)),
+  renderer.material({ baseColorMap: textureSDF, metallic: 0.8, roughness: 0.1 })
 ]);
 
 renderer.add(cube);
 
-const skybox = renderer.entity([
-  renderer.skybox({ sunPosition: [1, 1, 1] })
-]);
+const skybox = renderer.entity([renderer.skybox({ sunPosition: [1, 1, 1] })]);
 
 renderer.add(skybox);
 
@@ -38,7 +49,7 @@ renderer.add(reflectionProbe);
 
 function resize() {
   context.set({ width: innerWidth, height: innerHeight });
-  camera.getComponent('Camera').set({ aspect: innerWidth/innerHeight });
+  camera.set({ aspect: innerWidth/innerHeight });
   renderer.draw();
 }
 
